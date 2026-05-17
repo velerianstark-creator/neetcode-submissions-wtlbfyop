ddabe1426ca8f8afeeace16128975e415cb3ed42
velerianstark-creator <velerianstark@gmail.com>
2026-05-17 23:21:16 +0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        def add(current):
+            if not current:
+                return
+            if hashMap.get(current.val, None):
+                return hashMap.get(current.val)
+            new_node = Node(current.val)
+            hashMap[current.val] = new_node
+            
+            for neighbor in current.neighbors:
+                new_neighbor = add(neighbor)
+                if new_neighbor:
+                    new_node.neighbors.append(new_neighbor)
+            return new_node
+        hashMap = dict()
+        copy_graph = add(node)
+        return copy_graph
