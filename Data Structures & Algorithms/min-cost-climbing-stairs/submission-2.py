1676285a603348fdc720b26e320cb45feb64b583
velerianstark-creator <velerianstark@gmail.com>
2026-05-29 23:28:56 +0700

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        store = [0, 0]
+        for n in range(len(cost)+1):
+            if n == 0:
+                continue
+            if n == 1:
+                continue
+            twoStep = store[0] + cost[n-2]
+            oneStep = store[1] + cost[n-1]
+            store = [store[1], min(oneStep, twoStep)]
+        return store[1]
