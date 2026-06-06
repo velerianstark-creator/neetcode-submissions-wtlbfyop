5d82936f1de11bea4fa83425b6534824dbe4425a
velerianstark-creator <velerianstark@gmail.com>
2026-06-06 14:58:35 +0700

    Add: jump-game - submission-10

---
+++ Data Structures & Algorithms/jump-game/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        def jumpFromIndex(i):
+            if i not in memo:
+                if i >= len(nums) - 1:
+                    return True
+                if nums[i] == 0:
+                    memo.add(i)
+                    return False
+                for j in range(nums[i], 0, -1):
+                    if jumpFromIndex(i + j):
+                        return True
+            return False
+        memo = set()
+        return jumpFromIndex(0)
+        
